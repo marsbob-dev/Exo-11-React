d6fc9470436da6a9c39cc30042776eb9d8cb70e4
marsbob-dev <73162318+marsbob-dev@users.noreply.github.com>
2021-01-19 15:38:29 +0100

    exo terminé

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+state = {
+  compteur: 0,
+}
+
+incremente = () => {
+  this.setState({
+    compteur: this.state.compteur +1 
+  })
+}
+
+decremente = () => {
+  this.setState({
+    compteur: this.state.compteur -1 
+  })
+}
+
+swapClass (){
+  let span = document.getElementById("span");
+  span.classList.toggle("textRed")
+}
+  render ()
+ {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className="App">
+     <span id="span">{this.state.compteur}</span>
+     <button onClick={this.incremente}>Click +1</button>
+     <button onClick={this.decremente}>Click -1</button>
+     <button onClick={this.swapClass}>Swap red</button>
+   </div>
+ );
+  }
 }
 
 export default App;
